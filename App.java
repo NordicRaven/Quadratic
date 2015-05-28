94f9dff0cdfa1bfda73928e5736b9ca6741b8890
Vadym Stashuk <stahukvadim@gmail.com>
2015-05-28 14:58:42 +0200

    Change main signature

--- App.java
+++ App.java
@@ -1,59 +1,59 @@
-import java.util.Scanner;
-
-public class App {
-
-    private static class Params {
-        private final int a,b,c;
-
-        private Params(int a, int b, int c) {
-            this.a = a;
-            this.b = b;
-            this.c = c;
-        }
-    }
-
-    public static void main(String... args) {
-        System.out.println("This app will solve quadratic equations. ");
-        while (true) {
-            System.out.println("\nTo stop please enter \"stop\" " + "\nPlease enter values of a, b, c and" +
-                    " separate them with spaces " +
-                    "like this \"1 2 3\"");
-            try {
-                double[] result = Calculator.result(threeDoubles());
-                System.out.print("result is ---> ");
-                for (double v : result) {
-                    System.out.print(v + " ");
-                }
-                System.out.println();
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
-    }
-
-    static Params threeDoubles() {
-        double[] doubles = new double[3];
-        Scanner scanner = new Scanner(System.in);
-        for (int i = 0; i < 3; i++) {
-            boolean correctInput = false;
-            do {
-                if (scanner.hasNext("stop")) {
-                    System.out.println("Thank you Vadik, for writing such an excellent programm!");
-                    System.exit(0);
-                } else if (scanner.hasNextDouble()) {
-                    doubles[i] = scanner.nextDouble();
-                    System.out.println("you entered --> " + doubles[i]);
-                    correctInput = true;
-                } else {
-                    System.out.println("invalid input! please try again");
-                    scanner.next();
-                }
-
-            }
-            while (!correctInput);
-        }
-
-        return doubles;
-    }
-
-}
+//import java.util.Scanner;
+//
+//public class App {
+//
+//    private static class Params {
+//        private final int a,b,c;
+//
+//        private Params(int a, int b, int c) {
+//            this.a = a;
+//            this.b = b;
+//            this.c = c;
+//        }
+//    }
+//
+//    public static void main(String... args) {
+//        System.out.println("This app will solve quadratic equations. ");
+//        while (true) {
+//            System.out.println("\nTo stop please enter \"stop\" " + "\nPlease enter values of a, b, c and" +
+//                    " separate them with spaces " +
+//                    "like this \"1 2 3\"");
+//            try {
+//                double[] result = Calculator.result(threeDoubles());
+//                System.out.print("result is ---> ");
+//                for (double v : result) {
+//                    System.out.print(v + " ");
+//                }
+//                System.out.println();
+//            } catch (IllegalArgumentException e) {
+//                System.out.println(e.getMessage());
+//            }
+//        }
+//    }
+//
+//    static Params threeDoubles() {
+//        double[] doubles = new double[3];
+//        Scanner scanner = new Scanner(System.in);
+//        for (int i = 0; i < 3; i++) {
+//            boolean correctInput = false;
+//            do {
+//                if (scanner.hasNext("stop")) {
+//                    System.out.println("Thank you Vadik, for writing such an excellent programm!");
+//                    System.exit(0);
+//                } else if (scanner.hasNextDouble()) {
+//                    doubles[i] = scanner.nextDouble();
+//                    System.out.println("you entered --> " + doubles[i]);
+//                    correctInput = true;
+//                } else {
+//                    System.out.println("invalid input! please try again");
+//                    scanner.next();
+//                }
+//
+//            }
+//            while (!correctInput);
+//        }
+//
+//        return doubles;
+//    }
+//
+//}
---
+++ InputStreamLesson.java
@@ -0,0 +1,30 @@
+package Quadratic;
+import java.io.*;
+
+public class InputStreamLesson {
+
+    public static void main(String[] args) {
+        File fileFrom = new File("foto1.jpg");
+        File fileTo = new File("destination.jpg");
+        long fileLength = fileFrom.length();
+        System.out.println("number of bytes in image --> " + fileLength);
+
+        try (FileInputStream readBytes = new FileInputStream(fileFrom);
+             FileOutputStream outputStream = new FileOutputStream(fileTo)) {
+            byte[] byteArray = new byte[(int) fileLength];
+            int numOfBytes = readBytes.read(byteArray);
+            System.out.println("numOfBytes -->" + numOfBytes);
+            System.out.println(fileTo.createNewFile());
+            for (int i = 0; i < fileLength - 50000; i++) {
+                //if(i>1000){break;}
+                byte b = byteArray[i];
+                outputStream.write(b);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
